2ef7a771ee6e8290cdb3c7c96aed6f39aa27c79f
swaich-Techno <swaichtecho@gmail.com>
2026-06-03 23:46:18 +0530

    Delete app directory

--- app/staff/chat/page.jsx
+++
@@ -1,5 +0,0 @@
-import { redirect } from "next/navigation";
-
-export default function StaffChatAliasPage() {
-  redirect("/team/chat");
-}
--- app/staff/qrs/page.jsx
+++
@@ -1,5 +0,0 @@
-import { redirect } from "next/navigation";
-
-export default function StaffQrsAliasPage() {
-  redirect("/super-admin/businesses");
-}
