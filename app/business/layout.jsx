ebb90a948e22476179fdca3df588df2602081c73
swaich-Techno <swaichtecho@gmail.com>
2026-06-14 06:25:05 +0530

    Delete app directory

--- app/business/layout.jsx
+++
@@ -1,7 +0,0 @@
-export const metadata = {
-  robots: { index: false, follow: false }
-};
-
-export default function BusinessLayout({ children }) {
-  return children;
-}
--- app/super-admin/layout.jsx
+++
@@ -1,7 +0,0 @@
-export const metadata = {
-  robots: { index: false, follow: false }
-};
-
-export default function SuperAdminLayout({ children }) {
-  return children;
-}
